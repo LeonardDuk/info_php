614994a1e9f18f25198a327447273f14b70efcbe
LeonardDuk <106681793+LeonardDuk@users.noreply.github.com>
2023-07-11 21:56:03 -0300

    Create calculadora.php

---
+++ calculadora.php
@@ -0,0 +1,24 @@
+<?php
+ 
+  $a = "5";
+  $b = "7";
+ $operador = "*";
+ $resultado = "0";
+ 
+ if ($operador == "+") {
+    $resultado = $a + $b;
+ }
+
+ if ($operador == "-") {
+    $resultado = $a - $b ;
+ }
+ 
+if ($operador == "*") {
+    $resultado = $a *  $b ;
+}
+
+if ($operador == "/") {
+    $resultado =  $a / $b ;
+} 
+    
+echo $resultado; 
