66abd990e2106609bbccf5f582249900a8ec5a3a
LeonardDuk <leo554-bp@hotmail.com>
2023-08-01 21:29:47 -0300

    alterando a calculadora

--- calculadora.php
+++ calculadora.php
@@ -1,8 +1,8 @@
 <?php
  
-  $a = "100";
-  $b = "10";
- $operador = "perc";
+  $a = "2";
+  $b = "4";
+ $operador = "ma";
  $resultado = "0";
  
  if ($operador == "+") {
@@ -34,4 +34,8 @@ if ($operador == "perc") {
     $resultado = $a+($a * $b) / 100;
                   
 }
+
+if ( $operador == "ma") {
+    $resultado = ($a + $b)/2 ;
+} 
 echo $resultado; 
