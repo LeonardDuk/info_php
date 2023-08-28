79372f2b13a6c77d46a3adcbac5261a26f749069
LeonardDuk <leo554-bp@hotmail.com>
2023-08-28 20:17:24 -0300

    time-calculator

--- funcoes-aritimeticas/time-calculator.php
+++ funcoes-aritimeticas/time-calculator.php
@@ -24,11 +24,12 @@
    $totalFinalEmSegundos = $horaFinalEmSegundos + $minutosFinalEmSegundos + $segundosFinal;
 
    $tempoDoJogo = ($totalFinalEmSegundos - $totalInicialEmSegundos) /60;
-    echo "o Resultado final é: $tempoDoJogo";
+    //echo "o Resultado final é: $tempoDoJogo";
+    
 
     // calcular o tempo de jogo de um dia para o outro 
 
-    $acrescimos = 5;
+    $acrescimos = 0;
     $horaInicial = '23:00:00';
     $horaFinal = '00:30:00';
 
@@ -37,23 +38,28 @@
     $minutos = $arrHora[1];
     $segundos = $arrHora[2];
     
-    $diaemsegundos = $dia * 84600;          
+    $diaEmSegundos = 86400;          
     $horaEmSegundos = $hora * 3600;
     $minutosEmSegundos = $minutos * 60;
     
-    $totalInicialEmSegundos = $diaemsegundos + $horaEmSegundos + $minutosEmSegundos + $segundos;
+    $totalInicialEmSegundos =  $horaEmSegundos + $minutosEmSegundos + $segundos;
 
     $arrHora = explode(":", $horaFinal);
    $horaFinal = $arrHora[0];
    $minutosFinal = $arrHora[1];
    $segundosFinal = $arrHora[2];
 
-   $diaemsegundos = $dia * 84600;  
    $horaFinalEmSegundos = $horaFinal * 3600;
    $minutosFinalEmSegundos = $minutosFinal * 60;
    
-   $totalFinalEmSegundos =$diaemsegundos + $horaFinalEmSegundos + $minutosFinalEmSegundos + $segundosFinal ;
+   $totalFinalEmSegundos =  $horaFinalEmSegundos + $minutosFinalEmSegundos + $segundosFinal ;
 
-   $tempoDoJogo = ($totalInicialEmSegundos - $totalFinalEmSegundos) / 60  ;
-   $resultado =  $tempoDoJogo - 1260 ;
-   echo "<br>o Resultado é: $resultado<br>";
+   $tempoDoJogo = ($totalFinalEmSegundos - $totalInicialEmSegundos) / 60 ;
+
+   if ($tempoDoJogo <= 0 ) {
+      
+        $difinicial =  $diaEmSegundos - $totalInicialEmSegundos;
+        $tempoDoJogo = ($difinicial + $totalFinalEmSegundos)/60 ;
+   }
+    
+   echo "<br>o Resultado é: {$tempoDoJogo}<br>";
