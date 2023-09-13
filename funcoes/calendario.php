46de7958a6e74c1e36db2860a3d1342a478e6c09
LeonardDuk <leo554-bp@hotmail.com>
2023-09-12 21:15:33 -0300

    calendário

--- funcoes/calendario.php
+++ funcoes/calendario.php
@@ -1,21 +1,44 @@
 <?php
 
-$dataString = "29/05/1990";
+$dataString = "32/05/1990";
 $data = explode("/", $dataString);
 $dia = $data[0];
 $mês = $data[1];
 $ano = $data[2];
 
 
-function validardata($dia , $mês , $ano ) {
-    $dataString = "{$dia}/{$mês}/{$ano}";
 
-    if (($ano % 4 == 0) && ($ano % 100 != 0 || $ano % 400 == 0)) {
-        echo "{$dataString}  é bissexto";
-    } 
-    else {
-        echo "{$dataString} não é bissexto";
+
+    if ($dia < 1  || $dia > 31){
+        $dia = 1;
+        
     }
+    
+    if ($mês < 1  || $mês > 12){
+        $mês = 1;
+
+    }   
+     
+     $meses30dias = [4 , 6 , 9, 11];
+     $meses31dias = [1, 3 , 5 , 7 , 8 , 10 , 12];
+
+    if (in_array($mês,$meses30dias) && $dia > 30 ){
+        $dia = 1;
+        $mês += 1;
+    }
+
+    if (in_array($mês,$meses31dias) && $dia > 31 ){
+        $dia = 1;
+        $mês += 1;
+    }
+        if (in_array($mês,$meses30dias) && $dia > 30 ){
+            $dia = 31;
+            $mês -= 1;
+        }
+    
+        if (in_array($mês,$meses31dias) && $dia > 31 ){
+            $dia = 30;
+            $mês -= 1;
+    
 }
 
-validardata($dia , $mês , $ano );
