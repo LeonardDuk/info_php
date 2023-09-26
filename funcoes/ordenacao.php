4e95369adf3e6231d8171dc3dadfdc7f8268de53
LeonardDuk <leo554-bp@hotmail.com>
2023-09-26 19:54:22 -0300

    bubble sort , ordem crescente

--- funcoes/ordenacao.php
+++ funcoes/ordenacao.php
@@ -2,22 +2,22 @@
 //encontrar o maior valor em um array. 
 // E exibir esse valor ao final para o usuario.
 
-$numeros = [10 , 80, 5 , 14 ,90 , 29, 99 , 1 , 122];
-$n = count($numeros);
+// $numeros = [10 , 80, 5 , 14 ,90 , 29, 99 , 1 , 122];
+// $n = count($numeros);
 
- for ( $i =0; $i < $n ; $i++) {
-    if($i==0){
-        $maior = $numeros[0];
-    }
-    else {
-       if ($numeros[$i] > $maior){
+//  for ( $i =0; $i < $n ; $i++) {
+//     if($i==0){
+//         $maior = $numeros[0];
+//     }
+//     else {
+//        if ($numeros[$i] > $maior){
         
-      $maior = $numeros[$i];
-       } 
-    }
+//       $maior = $numeros[$i];
+//        } 
+//     }
    
- }
-echo "maior número é : {$maior}";
+//  }
+// echo "maior número é : {$maior}";
 
 
 /*
@@ -38,5 +38,5 @@ function bubble_sort($numeros){
   return $numeros;
 }
 
-
+echo implode("," , bubble_sort($numeros));
 
