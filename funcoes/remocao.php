ccaf02bbc26039299b958b2541db7798c091377d
LeonardDuk <leo554-bp@hotmail.com>
2023-09-26 21:54:29 -0300

    contagem

--- funcoes/remocao.php
+++ funcoes/remocao.php
@@ -1,13 +1,32 @@
 <?php
 //remover valores duplicados de um array.
 
-$frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "maçã" ];
+// $frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "maçã" , " maçã" ];
+// $n = count($frutas);
+// $newfrutas = [];
+// for ($i = 0 ; $i < $n; $i++){
+//    if (!in_array($frutas[$i], $newfrutas)){
+//     array_push($newfrutas , $frutas[$i]);
+//     }
+//     else {
+//         $newfrutas += 1 ;
+//     }
+// }
+
+//  echo implode ("," , $newfrutas);
+
+
+ $frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "maçã" , " maçã" ];
 $n = count($frutas);
 $newfrutas = [];
 for ($i = 0 ; $i < $n; $i++){
-   if (!in_array($frutas[$i], $newfrutas)){
-    array_push($newfrutas , $frutas[$i]);
-    
-   }
+    $fruta = $frutas[$i];
+       if (!isset($newfruta[$fruta])){
+     $newfrutas[$fruta] = 1 ;
+    }
+    else {
+        $newfrutas[$fruta] += 1;
+    }
 }
+
  echo implode ("," , $newfrutas);
