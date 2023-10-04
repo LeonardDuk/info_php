7f2b2b8bfa2cb3293b1f042d49c21f95bbc0fb89
LeonardDuk <leo554-bp@hotmail.com>
2023-10-03 21:51:28 -0300

    ordenação com funções

--- funcoes/remocao.php
+++ funcoes/remocao.php
@@ -16,20 +16,65 @@
 //  echo implode ("," , $newfrutas);
 
 
- $frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "maçã" , " maçã" ];
-$n = count($frutas);
-$newfrutas = [];
-for ($i = 0 ; $i < $n; $i++){
+//  $frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "uva" , "pera" , "maçã" , "maçã" ];
+// $n = count($frutas);
+// $newfrutas = [];
+// for ($i = 0 ; $i < $n; $i++){
+//     $fruta = $frutas[$i];
+//        if (!isset($newfrutas[$fruta])){
+//      $newfrutas[$fruta] = 1 ;
+//     }
+//     else {
+//         $newfrutas[$fruta] += 1;
+//     }
+//     if (!in_array($frutas[$i], $newfrutas)){
+//              array_push($newfrutas , $frutas[$i]);
+//             }
+// }
+
+//  echo implode (" " , $newfrutas);
+
+
+ //Utilizar o mesmo exercício e criar uma função que recebe 
+ // Dois parametros (array, string) para ordenar o array de frutas(1° parametro)
+ // em ordem crescente(ASC) or decrescente(DESC)
+
+ $frutas = ["maçã" , "banana" , "laranja" , "banana" , "uva" , "uva" , "pera" , "maçã" , "maçã" ];
+ 
+ $n = count($frutas);
+
+ $newfrutas = [];
+
+ for ($i = 0 ; $i < $n; $i++){
     $fruta = $frutas[$i];
-       if (!isset($newfrutas[$fruta])){
-     $newfrutas[$fruta] = 1 ;
+
+    if (!isset($newfrutas[$fruta])){
+        $newfrutas[$fruta] = 1 ;
     }
     else {
         $newfrutas[$fruta] += 1;
     }
-    if (!in_array($frutas[$i], $newfrutas)){
-             array_push($newfrutas , $frutas[$i]);
-            }
 }
 
- echo implode ("," , $newfrutas);
+foreach ($newfrutas as $fruta => $value) {
+    echo "{$fruta} {$value}<br>";
+}
+
+function ordencao($newfrutas, $ordem = "asc") {
+
+    if ($ordem == "asc") {
+
+    } else {
+        rsort($newfrutas);
+    }
+
+    return $newfrutas;
+}
+
+$ordem ="desc";
+$frutas = ordenacao($newfrutas, $ordem);
+
+foreach ($frutas as $i => $value) {
+    echo "{$i} {$value} <br>";
+}
+ 
